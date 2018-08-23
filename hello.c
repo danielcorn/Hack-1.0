e3fee755a776f9f302a5bce76bf47c31282b5272
Jarrod Blaser <j-rod2@live.com>
2018-08-23 16:11:39 -0500

    changes made Jarrod

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Daniel Cornelius, Electric Engineering\n");
+  printf("Daniel Cornelius, Electric Engineering, Jarrod Blaser, Mechanical Engineering\n");
 
   return 0;
 }
