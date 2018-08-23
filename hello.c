c11dcfc212c291566929c9f80fd16d6bfd1759dc
danielcorn <42561743+danielcorn@users.noreply.github.com>
2018-08-23 15:57:43 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Daniel Cornelius
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Daniel Cornelius, Electric Engineering\n");
+
+  return 0;
+}
